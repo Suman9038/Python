8c9a40b47599a82cabbba91626d54a5ab6a6f266
Suman9038 <sumanjhad903878@gmail.com>
2024-09-27 01:28:28 +0530

    added schema not in database but in local memory

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app=FastAPI()
+
+class Post(BaseModel) :
+    title : str
+    content : str
+    published : bool = True 
+    rating: Optional[int]=None
+
+my_posts=[{"title":"HI Guys I am Suman","content":"I am Pursuing Btech","id":1},
+          {"title":"HI Guys I am Ankit","content":"I am Pursuing BCA","id":2}]
+
+def find_post(id) :
+    for i in my_posts :
+        if i['id'] == id :
+            return i
 
 @app.get("/")
 def root () :
     return{"Message":"Hello world"}
+
+@app.get("/posts")
+def get_Post() :
+    return{"Data":my_posts}
+
+@app.post("/posts")
+def createPost(new_post: Post) :
+    #  print(new_post.published)
+    #  print(new_post.rating)
+    # print(new_post)
+    # print(new_post.dict())
+    next_post=new_post.dict() 
+    next_post['id']=randrange(0,100000)
+    my_posts.append(next_post)
+    return {"data":next_post}
+
+@app.get("/posts/{id}")
+def get_Data(id:int ) :
+    post =find_post(int(id))
+    return{"data" : post}
