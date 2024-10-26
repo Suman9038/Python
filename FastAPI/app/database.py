752818ad19f1f8ae039c5152c455f64a647608c2
Suman9038 <sumanjhad903878@gmail.com>
2024-10-27 04:25:47 +0530

    Created Database connection

---
+++ FastAPI/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Database_URL="mysql://root:Suman2003@localhost/fastapi"
+
+engine= create_engine(Database_URL)
+
+Sessionlocal= sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base=declarative_base()
+
+def get_db() :
+    db=Sessionlocal() 
+    try :
+        yield db
+    finally :
+        db.close()
