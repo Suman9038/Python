1b1bd750eb18d81c21a5670c69caf070f04d39f9
Suman9038 <sumanjhad903878@gmail.com>
2024-10-31 03:58:28 +0530

    Added User Table For User Login

--- FastAPI/app/models.py
+++ FastAPI/app/models.py
@@ -2,6 +2,7 @@ from .database import Base
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from sqlalchemy import Column,Integer,String,Boolean
+import random
 
 class Post(Base) :
     __tablename__="posts"
@@ -11,3 +12,13 @@ class Post(Base) :
     content=Column(String(255),nullable=False)
     published=Column(Boolean,server_default=text("True"))
     created_at=(Column(TIMESTAMP,nullable=False,server_default=text('now()')))
+
+
+class User(Base) :
+    __tablename__="users"
+    
+    id=Column(Integer,primary_key=True,nullable=False,default=lambda : random.randint(1,100000))
+    email=Column(String(255),nullable=False,unique=True)
+    password=Column(String(50),nullable=False)
+    created_at=Column(TIMESTAMP,nullable=False,server_default=text('now()'))
+    
